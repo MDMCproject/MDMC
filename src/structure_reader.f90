f5a0ebb5ece6667c60cf66b00b3845ea36fd99ab
Anders-Markvardsen <anders.markvardsen@stfc.ac.uk>
2004-12-15 17:54:21 +0000

    Add code to read coordinates
    git-svn-id: file:///home/osboxes/Desktop/svnTogit/svnMDMC/trunk@9 788f9de0-bab7-471b-80ca-8898065b70ba

--- src/structure_reader.f90
+++ src/structure_reader.f90
@@ -1,21 +1,34 @@
 module structure_reader_class
 use flib_sax
-use common_block_class ! , only : common_configuration
-!use moldyn_control_class
-!use structure_reader_class
+use common_block_class, only : common_configuration
+
 
   implicit none
   
   private :: begin_element, end_element, pcdata_chunk
   private :: start_document, end_document
   
+  integer, private :: count_number_atoms
 
 contains
 
   subroutine make_structure(filename)
     character(len=*), intent(in) :: filename
 
-    write(*,*) "fisse"
+    type(xml_t) :: fxml
+    integer     :: iostat
+
+
+    call open_xmlfile(filename,fxml,iostat)
+    if (iostat /= 0) stop "Cannot open file."
+
+    call xml_parse(fxml,           &
+                 begin_element_handler=begin_element, &
+                 end_element_handler=end_element,     &
+                 pcdata_chunk_handler=pcdata_chunk, &
+                 start_document=start_document, &
+                 end_document=end_document)
+
   end subroutine make_structure
 
   !START_DOCUMENT
@@ -28,17 +41,39 @@ contains
     character(len=*), intent(in)   :: name
     type(dictionary_t), intent(in) :: attributes
     
-    integer :: status
-    character(len=40) :: control_object_name, read_number_atoms
+    integer :: status, ndata=0
+    character(len=40) :: control_object_name, read_number_atoms, read_dp
+    integer :: number_atoms(1);
     character(len=120) :: filename
+    double precision :: number_dp(1)
 
     select case(name)
       case("molecule")
         call get_value(attributes,"number", read_number_atoms,status)
-        !allocate(common_configuration%cf_structure%atoms(number_atoms))        
-        !call make_structure(filename)
-        ! get attributes
-        ! my_configuration%my_structure = make_structure()
+        call build_data_array(read_number_atoms, number_atoms, ndata)
+        write(*,*) number_atoms
+        allocate(common_configuration%cf_structure%atoms(number_atoms(1)))
+        count_number_atoms = 1
+
+      case("atom")
+        call get_value(attributes,"x3", read_dp,status)
+        ndata = 0
+        call build_data_array(read_dp, number_dp, ndata)
+        common_configuration%cf_structure%atoms(count_number_atoms)%position(1) = number_dp(1)
+        call get_value(attributes,"y3", read_dp,status)
+        ndata = 0
+        call build_data_array(read_dp, number_dp, ndata)
+        common_configuration%cf_structure%atoms(count_number_atoms)%position(2) = number_dp(1)
+        call get_value(attributes,"z3", read_dp,status)
+        ndata = 0
+        call build_data_array(read_dp, number_dp, ndata)
+        common_configuration%cf_structure%atoms(count_number_atoms)%position(3) = number_dp(1)
+        write (*,*) count_number_atoms, &
+          common_configuration%cf_structure%atoms(count_number_atoms)%position(1), &
+          common_configuration%cf_structure%atoms(count_number_atoms)%position(2), &
+          common_configuration%cf_structure%atoms(count_number_atoms)%position(3)
+        count_number_atoms = count_number_atoms + 1        
+
 	
     end select
 
