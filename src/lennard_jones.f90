b169cc4260a2fadf9cbeda3b3556be29d2910b4e
Anders-Markvardsen <anders.markvardsen@stfc.ac.uk>
2005-11-07 16:00:23 +0000

    forgot to add lennard_jones.f90 in previous and pe_containers.f90 by mistake (which is here deleted again)
    git-svn-id: file:///home/osboxes/Desktop/svnTogit/svnMDMC/trunk@54 788f9de0-bab7-471b-80ca-8898065b70ba

---
+++ src/lennard_jones.f90
@@ -0,0 +1,66 @@
+module lennard_jones_class
+use various_constants_class
+use structure_class
+use variable_class
+
+implicit none
+
+  type lj_pe_container
+    type (variable), dimension(:), allocatable :: vars
+  end type lj_pe_container
+
+
+contains
+
+  function lj_val(str, container) result (val)
+    type (structure), intent(in) :: str
+		type (lj_pe_container), intent(in) :: container
+    real (db) :: val
+  
+    integer :: i1, i2, n_tot, i
+    real (db) :: r_cut, rr_cut 
+    real (db) :: sigma, epsilon, epsilon_times4
+    real (db) :: rr, rri, rri3
+		real (db), dimension(ndim) :: diff_vec
+    
+    
+		sigma = container%vars(1)%val
+		epsilon = container%vars(2)%val
+    epsilon_times4 = 4*epsilon
+    
+    r_cut = 2**0.166666666666667 * sigma
+    rr_cut = r_cut * r_cut
+    
+		n_tot = size(str%atoms)
+    
+    val = 0.0
+    
+		do i1 = 1, n_tot
+		  do i2 = i1+1, n_tot
+        diff_vec = str%atoms(i1)%r - str%atoms(i2)%r
+        
+        do i = 1, ndim
+          if (diff_vec(i) >= 0.5 * str%box_edges(i)) then
+            diff_vec(i) = diff_vec(i) - str%box_edges(i)
+          end if
+          if (diff_vec(i) < -0.5 * str%box_edges(i)) then
+            diff_vec(i) = diff_vec(i) + str%box_edges(i)
+          end if       
+        end do
+        
+        rr = sum(diff_vec*diff_vec)
+        
+        if (rr < rr_cut) then
+          rri = sigma*sigma / rr
+          rri3 = rri * rri * rri
+          val = val + epsilon_times4 * rri3 * (rri3 - 1.0) + 1.0   
+        end if
+			
+			end do
+		end do
+        
+  end function lj_val
+  
+    
+    
+end module lennard_jones_class
