41264e158390716f84249390a3c7734f343463e0
Anders-Markvardsen <anders.markvardsen@stfc.ac.uk>
2005-11-08 07:04:10 +0000

    merged umbrello design
    git-svn-id: file:///home/osboxes/Desktop/svnTogit/svnMDMC/trunk@55 788f9de0-bab7-471b-80ca-8898065b70ba

--- src/phasespace.f90
+++ src/phasespace.f90
@@ -16,7 +16,20 @@ use common_potential_block_class
 
   type phasespace
     type (structure) :: str
+    real(db), dimension(:,:), allocatable :: p  ! momenta
+    real(db), dimension(:,:), allocatable :: deriv  ! derivs(=- force = -dp/dt)    
   
+    ! the position coordinates are here stored in the same format as p and
+    ! deriv for coding convenience and probably a slight speed gain
+    real(db), dimension(:,:), allocatable :: r  ! atom positions
+    
+    ! this one is again added for speed gain and convenience
+    real(db), dimension(:,:), allocatable :: inv_mass 
+    
+    ! this one to e.g. later speed up the calculation of the kinetic energy
+    ! p_div_mass stands for momentum divided by mass
+    real(db), dimension(:,:), allocatable :: p_div_mass
+    
     type (near_neighb_list) :: neighb_list
   end type phasespace
 
@@ -28,7 +41,38 @@ contains
     integer, intent(in) :: n_md
     real(db), intent(in) :: delta_t   
     
-    real(db) :: pot_energy
+    real(db) :: pot_energy, delta_t_half
+    integer :: i
+    
+    delta_t_half = delta_t / 2.0
+    
+    ! leap-frog algorithm version used in Rapaport, where it is assumed
+    ! that the derivatives are up-to-date
+    
+    do i = 1, n_md-1
+      ! update momenta to time t=t+h/2 assuming we have up-to-date derivs 
+      ! for time t=t
+      ps%p = ps%p - delta_t_half * ps%deriv
+      
+      ! update positions to time t=t+h and store p_div_mass which can be used 
+      ! for e.g. calculating the kinetic energy a bit faster!
+      ps%p_div_mass = ps%p * ps%inv_mass
+      ps%r = ps%r + delta_t * ps%p_div_mass
+      
+      ! calculate derivatives for time t=t+h
+      
+      ! now get momenta in sync with positions and ready for the next loop
+      ps%p = ps%p - delta_t_half * ps%deriv      
+    end do
+    
+    ! do exactly the same as in the loop above but this time calculate
+    ! the derivatives + core-loop-md-extra
+    
+    ps%p = ps%p - delta_t_half * ps%deriv    
+    ps%p_div_mass = ps%p * ps%inv_mass
+    ps%r = ps%r + delta_t * ps%p_div_mass    
+    
+    ps%p = ps%p - delta_t_half * ps%deriv
     
     pot_energy = gpe_val(ps%str, common_gpe)
                          
@@ -48,6 +92,14 @@ contains
     ps%neighb_list = make_near_neighb_list(str, r_cut, delta_r)
     
     ps%str = copy_structure(str)
+    
+    allocate(ps%p(n_tot,3))
+    allocate(ps%deriv(n_tot,3))
+    allocate(ps%r(n_tot,3))
+    allocate(ps%inv_mass(n_tot,3))
+    allocate(ps%p_div_mass(n_tot,3))
+    
+    ! add coordinates to 
     
   end function make_phasespace
 
