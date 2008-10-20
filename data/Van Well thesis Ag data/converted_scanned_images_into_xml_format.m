004145a19e94d128f6eecfbf9c23601d8747c393
Anders-Markvardsen <anders.markvardsen@stfc.ac.uk>
2008-10-20 14:15:39 +0000

    Added Van Well thesis Ag data, which according to Robert might be a bit doughy.
    git-svn-id: file:///home/osboxes/Desktop/svnTogit/svnMDMC/trunk@165 788f9de0-bab7-471b-80ca-8898065b70ba

---
+++ data/Van Well thesis Ag data/converted_scanned_images_into_xml_format.m
@@ -0,0 +1,146 @@
+% read in 1st column
+
+fid = fopen('table_V_column1.txt','r');
+
+num_omega_1 = 12;
+S_Q_Omega_1 = zeros([1 num_omega_1]);
+S_Q_Omega_1 = NaN;
+sigma_S_Q_Omega_1 = zeros([1 num_omega_1]);
+sigma_S_Q_Omega_1 = NaN;
+
+for i = 1 : num_omega_1
+  dummy = fgetl(fid);
+  
+  fisse = findstr(dummy, ')');
+  fisse_begin = findstr(dummy, '(');
+  
+  S_Q_Omega_1(1, i) = str2double(dummy(1:6));
+  sigma_S_Q_Omega_1(1, i) = str2double(dummy((fisse_begin+1):(fisse-1)))*0.0001;
+end
+  
+fclose(fid);
+
+
+% read in 2st column
+
+fid = fopen('table_V_column2.txt','r');
+
+num_omega_2 = 29;
+S_Q_Omega_2 = zeros([1 num_omega_2]);
+S_Q_Omega_2 = NaN;
+sigma_S_Q_Omega_2 = zeros([1 num_omega_2]);
+sigma_S_Q_Omega_2 = NaN;
+
+for i = 1 : num_omega_2
+  dummy = fgetl(fid);
+  
+  fisse = findstr(dummy, ')');
+  fisse_begin = findstr(dummy, '(');
+  
+  S_Q_Omega_2(1, i) = str2num(dummy(1:6));
+  sigma_S_Q_Omega_2(1, i) = str2num(dummy((fisse_begin+1):(fisse-1)))*0.0001;
+end
+  
+fclose(fid);
+
+
+% read in column 3 to 8
+
+fid = fopen('table_V_column3_to_10.txt','r');
+
+num_omega_3 = 37;
+S_Q_Omega_3 = zeros([8 num_omega_3]);
+S_Q_Omega_3 = NaN;
+sigma_S_Q_Omega_3 = zeros([8 num_omega_3]);
+sigma_S_Q_Omega_3 = NaN;
+
+i_omega = 0;
+while 1
+  dummy = fgetl(fid);
+  
+  if dummy == -1
+    break;
+  end
+  
+  i_q = 1;
+  i_omega = i_omega + 1;
+  
+  fisse = findstr(dummy, ')');
+  fisse_begin = findstr(dummy, '(');
+  
+  S_Q_Omega_3(i_q, i_omega) = str2num(dummy(1:6));
+  sigma_S_Q_Omega_3(i_q, i_omega) = str2num(dummy((fisse_begin(1)+1):(fisse(1)-1)))*0.0001;
+  
+  for i_q = 2 : 8
+    %kus = str2num(dummy((fisse(i_q-1)+2):(fisse(i_q-1)+2+5)))
+    S_Q_Omega_3(i_q, i_omega) = str2num(dummy((fisse(i_q-1)+2):(fisse(i_q-1)+2+5)));
+    sigma_S_Q_Omega_3(i_q, i_omega) = str2num(dummy((fisse_begin(i_q)+1):(fisse(i_q)-1)))*0.0001;
+  end
+  
+end
+
+fclose(fid);
+
+
+% put everything in one matrix
+
+S_Q_Omega = zeros([10 num_omega_3]);
+%S_Q_Omega = NaN;
+sigma_S_Q_Omega = zeros([10 num_omega_3]);
+%sigma_S_Q_Omega = NaN;
+
+S_Q_Omega(1,1:num_omega_1) = S_Q_Omega_1;
+S_Q_Omega(1,num_omega_1+1:end) = NaN;
+S_Q_Omega(2,1:num_omega_2) = S_Q_Omega_2;
+S_Q_Omega(2,num_omega_2+1:end) = NaN;
+S_Q_Omega(3:10,1:num_omega_3) = S_Q_Omega_3;
+
+sigma_S_Q_Omega(1,1:num_omega_1) = sigma_S_Q_Omega_1;
+sigma_S_Q_Omega(1,num_omega_1+1:end) = NaN;
+sigma_S_Q_Omega(2,1:num_omega_2) = sigma_S_Q_Omega_2;
+sigma_S_Q_Omega(2,num_omega_2+1:end) = NaN;
+sigma_S_Q_Omega(3:10,1:num_omega_3) = sigma_S_Q_Omega_3;
+
+
+% Create the XML representation of Table V
+
+% q values as read from Table V converted to inverse AA
+q = [4.2 8.4 12.6 16.2 19.2 22.2 25.8 30.0 34.2 39.0]/10;
+% omega values as read from Table V in units of ps
+omega = [0.0:0.2:2.0];
+omega = [omega 2.5:0.5:10.0];
+omega = [omega 11:1:20];
+omega = omega / 10.0;  % to convert from 1/ps to 1/[10^-13 s]
+
+docNode = com.mathworks.xml.XMLUtils.createDocument('S_q_omega')
+docRootNode = docNode.getDocumentElement;
+docRootNode.setAttribute('n-omega-points' , num2str(length(omega)));
+docRootNode.setAttribute('n-q-points' , num2str(length(q)));
+docRootNode.setAttribute('q-unit' , 'AA^-1');
+docRootNode.setAttribute('omega-unit' , '1/[10^-13 s]');
+thisElement = docNode.createElement('description');
+thisElement.appendChild... 
+    (docNode.createTextNode(['Copy of data from A. A. Van Well thesis.' ...
+    ' Table V page 41-2. Ag S(Q,Omega) data.']));
+docRootNode.appendChild(thisElement); 
+for j = 1 : length(omega)
+  for i = 1 : length(q)
+    omegaElement = docNode.createElement('SQomega');
+    omegaElement.setAttribute('q', num2str(q(i)));
+    omegaElement.setAttribute('omega', num2str(omega(j)));
+    if isnan(S_Q_Omega(i,j))
+      omegaElement.setAttribute('S', 'no data');     
+    else
+      omegaElement.setAttribute('S', num2str(S_Q_Omega(i,j)));
+      omegaElement.setAttribute('error', num2str(sigma_S_Q_Omega(i,j)));
+    end
+    thisElement.appendChild(omegaElement);
+  end
+  docRootNode.appendChild(thisElement);
+end
+ 
+% Save the sample XML document.
+xmlFileName = 'Well s_q_omega Ag data.xml';
+xmlwrite(xmlFileName,docNode);
+%edit(xmlFileName);
+mesh(q,omega, S_Q_Omega')
---
+++ data/Van Well thesis Ag data/read_fisse.m
@@ -0,0 +1,74 @@
+function s_q_omega_info = read_s_q_omega(filename)
+
+s = xmlread(filename);
+
+mylist = s.getElementsByTagName('SQomega');
+
+q_temp = zeros([1 mylist.getLength()]);
+omega_temp = zeros([1 mylist.getLength()]);
+Ss_temp = zeros([1 mylist.getLength()]);
+Sd_temp = zeros([1 mylist.getLength()]);
+
+n_q = 0; 
+last_q = -1000; % q should never be negative I believe
+for i = 0 : mylist.getLength()-1
+  q_temp(i+1) = str2num(mylist.item(i).getAttribute('q'));
+  omega_temp(i+1) = str2num(mylist.item(i).getAttribute('omega'));
+  Ss_temp(i+1) = str2num(mylist.item(i).getAttribute('S-self'));
+  Sd_temp(i+1) = str2num(mylist.item(i).getAttribute('S-diff'));
+  if q_temp(i+1) > last_q
+    n_q = n_q + 1;
+    last_q = q_temp(i+1);
+  end
+end 
+
+top_element = s.getElementsByTagName('s-q-omega');
+
+n_time = length(q_temp) / n_q;
+
+q = q_temp(1:n_q);
+omega = zeros([1 n_time]);
+S_d = zeros([n_q n_time]);
+S_s = zeros([n_q n_time]);
+
+time_index = 1;
+for j = 1 : n_time
+  q_index = 1;
+  omega(time_index) = omega_temp(n_q*(j-1)+1);
+  for i = 1 : n_q   
+    S_d(q_index, time_index) = Sd_temp(n_q*(j-1)+i);
+    S_s(q_index, time_index) = Ss_temp(n_q*(j-1)+i);
+    q_index = q_index + 1;
+  end
+  time_index = time_index + 1;
+end
+
+
+if nargout == 1
+  s_q_omega_info.S_s = S_s;
+  s_q_omega_info.S_d = S_d;
+  s_q_omega_info.q = q;
+  s_q_omega_info.omega = omega;
+ 
+  return;
+end
+
+
+%subplot(1,3,1)
+%surf(q, omega, S_d')
+%xlabel('q [AA\^-1]')
+%ylabel('\omega 1/[10\^-13 s]')
+%zlabel('S_d (q,\omega)')
+%title(char(top_element.item(0).getAttribute('title')))
+
+%subplot(1,3,2)
+%surf(q, omega, S_s')
+%xlabel('q [AA\^-1]')
+%ylabel('\omega 1/[10\^-13 s]')
+%zlabel('S_s (q,\omega)')
+
+%subplot(1,3,3)
+surf(q, omega, S_s'+S_d')
+xlabel('q [AA\^-1]')
+ylabel('\omega 1/[10\^-13 s]')
+zlabel('S_s + S_d')
